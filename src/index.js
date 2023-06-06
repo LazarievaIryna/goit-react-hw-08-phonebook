3cf55b43aa061380640ece8f155c3b11cf150950
Iryna Lazareva <il337733@gmail.com>
2023-06-06 09:21:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {App} from './components/App/App';
+// import {App} from './components/App/App';
 import './index.css';
-import { store } from './redux/store.js';
-import { Provider } from 'react-redux';
+// import { store } from './redux/store.js';
+// import { Provider } from 'react-redux';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       
-        <App />
+        {/* <App /> */}
      
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
