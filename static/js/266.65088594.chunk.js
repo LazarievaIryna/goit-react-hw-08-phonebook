7298f564ac69316d6174aa91b46bb0992acdee96
LazarievaIryna <99398268+LazarievaIryna@users.noreply.github.com>
2023-06-07 10:42:08 +0000

    Deploying to gh-pages from @ LazarievaIryna/goit-react-hw-08-phonebook@1f008633c61c6b6f7e657c0dd695d39223e9fe02 🚀

--- static/js/266.65088594.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,r,t){t.r(r),t.d(r,{Home:function(){return c}});var n=t(184),c=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"Welcome page"})})}}}]);
-//# sourceMappingURL=266.65088594.chunk.js.map
