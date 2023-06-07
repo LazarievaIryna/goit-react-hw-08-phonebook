24317818fc994761011487f2cbdbce87191afc8e
Iryna Lazareva <il337733@gmail.com>
2023-06-07 09:45:57 +0300

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -5,19 +5,24 @@ const initialState = {
     user: { name: null, email: null },
     token: null,
     isLoggedIn: false,
-    isRefreshing: false,
+    // isRefreshing: false,
   };
 
   const authSlice = createSlice({
     name: 'auth',
     initialState,
-    extraReducers: builder=>{
-        builder
-        .addCase(register.fulfilled, (state, action)=>{
-            state.user=action.payload.user;
-            state.token = action.payload.token;
+    // extraReducers: builder=>{
+    //     builder
+    //     .addCase(register.fulfilled, (state, action)=>{
+    //         state.user=action.payload.user;
+    //         state.token = action.payload.token;
+    //         state.isLoggedIn = true;
+    //     })
+    // }
+    [register.fulfilled](state, action){
+      state.user=action.payload.user;
+         state.token = action.payload.token;
             state.isLoggedIn = true;
-        })
     }
   });
   
